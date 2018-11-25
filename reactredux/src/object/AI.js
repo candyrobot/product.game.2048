c2bcf5ee39ad9554a238270deb51e21e47be7997
candyrobot <findwkwk@gmail.com>
2018-11-25 22:58:57 +0900

    #1

--- reactredux/src/object/AI.js
+++ reactredux/src/object/AI.js
@@ -5,44 +5,44 @@ export default class AI {
     var timer = setInterval(()=> {
       var method = this.getMethodBest();
       method === undefined ? clearInterval(timer) : this.gm.play(method);
-    }, 50);
+    }, 1000);
   }
 
   getMethodBest() {
     var result = undefined;
     var score = {
-      left: undefined,
+      bottom: undefined,
       top: undefined,
       right: undefined,
-      bottom: undefined,
+      left: undefined,
     };
 
     if(!this.gm.isUnchangedTo(this.gm.mergeToLeft()))
       score.left =
       this.getMergableScore(this.gm.mergeToLeft({ isReturnMapJustMerged: true }))
-       - this.getDangerScore(this.gm.mergeToLeft({ isReturnMapJustMerged: true }))
-      // - this.getDeadSpaceScore(this.gm.mergeToLeft({ isReturnMapJustMerged: true }))
+      - this.getDangerScore(this.gm.mergeToLeft({ isReturnMapJustMerged: true }))
+      - this.getDeadSpaceScore(this.gm.mergeToLeft({ isReturnMapJustMerged: true }))
       ;
 
     if(!this.gm.isUnchangedTo(this.gm.mergeToTop()))
       score.top =
       this.getMergableScore(this.gm.mergeToTop({ isReturnMapJustMerged: true }))
-       - this.getDangerScore(this.gm.mergeToTop({ isReturnMapJustMerged: true }))
-      // - this.getDeadSpaceScore(this.gm.mergeToTop({ isReturnMapJustMerged: true }))
+      - this.getDangerScore(this.gm.mergeToTop({ isReturnMapJustMerged: true }))
+      - this.getDeadSpaceScore(this.gm.mergeToTop({ isReturnMapJustMerged: true }))
       ;
 
     if(!this.gm.isUnchangedTo(this.gm.mergeToRight()))
       score.right =
       this.getMergableScore(this.gm.mergeToRight({ isReturnMapJustMerged: true }))
-       - this.getDangerScore(this.gm.mergeToRight({ isReturnMapJustMerged: true }))
-      // - this.getDeadSpaceScore(this.gm.mergeToRight({ isReturnMapJustMerged: true }))
+      - this.getDangerScore(this.gm.mergeToRight({ isReturnMapJustMerged: true }))
+      - this.getDeadSpaceScore(this.gm.mergeToRight({ isReturnMapJustMerged: true }))
       ;
 
     if(!this.gm.isUnchangedTo(this.gm.mergeToBottom()))
       score.bottom =
       this.getMergableScore(this.gm.mergeToBottom({ isReturnMapJustMerged: true }))
-       - this.getDangerScore(this.gm.mergeToBottom({ isReturnMapJustMerged: true }))
-      // - this.getDeadSpaceScore(this.gm.mergeToBottom({ isReturnMapJustMerged: true }))
+      - this.getDangerScore(this.gm.mergeToBottom({ isReturnMapJustMerged: true }))
+      - this.getDeadSpaceScore(this.gm.mergeToBottom({ isReturnMapJustMerged: true }))
       ;
 
     console.log(score);
@@ -86,15 +86,20 @@ export default class AI {
     var score = 0;
     map.forEach((a, y)=> {
       if(a[a.length-1].value===0 && a.filter((dat)=> dat.value !== 0 ).length >= 3)
-        score = score + 8;
+        score = score + 1;
     });
     return score;
+  }
+
+  // ２つ先の手でmergeの数
+  xxxxx() {
+
   }
 
   getMergableScore(map, option = {}) {
     var score = 0;
     option.doWhenMerged = function() {
-      score = score + 4;
+      score = score + 1;
       console.log('can');
     };
     map.mapAll((v,p,map)=> this.gm.mergeBy2048(v,p.x,map[p.y],option));
